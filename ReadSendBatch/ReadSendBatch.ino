9104908463e5c46671b521cb92e75642301e9965
LINPOYU <t76580934@gmail.com>
2017-04-25 22:06:48 +0800

    Add files via upload

---
+++ ReadSendBatch/ReadSendBatch.ino
@@ -0,0 +1,36 @@
+#include <stdint.h>
+
+const int MaxSampleCount = 5000;
+
+char buffer[100];
+
+
+void setup() {
+  SerialUSB.begin(0);
+  while(!SerialUSB);
+  SerialUSB.flush( );
+}
+
+void loop() {
+  //Wait until Matlab signals start
+  while(SerialUSB.read() == -1);
+  ReadSendBatch(); // Batch send
+}
+  
+void ReadSendBatch()
+{
+  static uint16_t samples[MaxSampleCount];
+  static int sampleCount = 0;
+  while (sampleCount < MaxSampleCount) {
+    samples[sampleCount++] = analogRead(A0);
+    //delayMicroseconds(1);
+    //delay(1);
+  }
+  
+  for (int i = 0 ; i < MaxSampleCount ; i++) {
+    sprintf(buffer, "%d\r\n", samples[i]);
+    SerialUSB.print(buffer);
+    //Serial.print(buffer);
+  }
+  
+}
